1da33d680d6ba33a5b4941653ac3857c6d910429
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-17 21:36:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+ let draggedElement = null;
+
+        document.querySelectorAll('.image').forEach(img => {
+            img.addEventListener('dragstart', (e) => {
+                draggedElement = e.target;
+            });
+            img.addEventListener('dragover', (e) => {
+                e.preventDefault();
+            });
+            img.addEventListener('drop', (e) => {
+                e.preventDefault();
+                if (draggedElement !== e.target) {
+                    let tempBg = draggedElement.style.backgroundImage;
+                    draggedElement.style.backgroundImage = e.target.style.backgroundImage;
+                    e.target.style.backgroundImage = tempBg;
+                }
+            });
+        });
